66ee03061ab55ae52b6509432a182a67273803d5
Max Pearce Basman <pearcebasmanm@proton.me>
2026-02-13 15:42:17 -0500

    Add cli library and minimal template

--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from soen321-project!")
+import click
+
+
+@click.group()
+def cli():
+    """TODO: Project Name
+
+    TODO: Command Description
+    """
+
+
+@cli.command()
+def encrypt():
+    """
+    TODO: document
+    """
+    # Actual algorithms should not go here
+    # They should be defined in separate files and called from here
+    print("TODO: implement encryption")
+    pass
+
+
+@cli.command()
+def decrypt():
+    """
+    TODO: document
+    """
+    print("TODO: implement decryption")
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    cli()
